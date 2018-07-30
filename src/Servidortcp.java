d4bd972c33c503dda2013a0cc807197cf276f10a
Unknown <josebp_25@hotmail.com>
2018-07-30 02:12:45 -0400

    server

--- src/Servidortcp.java
+++ src/Servidortcp.java
@@ -1,9 +1,30 @@
-
+import java.net.*;
+import java.io.*;
 public class Servidortcp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ServerSocket socket;
+		
+		boolean fin = false;
+		
+		try{
+			socket= new ServerSocket(6000);
+		Socket socket_client = socket.accept();
+		
+		DataInputStream in = new DataInputStream(socket_client.getInputStream());
+		
+		do{
+			String message = "";
+			message = in.readUTF();
+			System.out.println(message);
+			
+		}while(1>0);
 
+	}catch (Exception g){
+		System.err.println(g.getMessage());
+		System.exit(1);
 	}
 
 }
+	}
